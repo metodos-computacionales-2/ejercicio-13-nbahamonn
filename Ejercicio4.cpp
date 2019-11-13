766e7a934ff2ce5fabe3501c7aec818948bdb7bd
Default user <n.bahamonn@uniandes.edu.co>
2019-11-13 04:44:57 +0000

    Se cambia el nombre a ambas funciones y dan los mismos resultados

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std; 
 
-int getMaxInt(int a, int b);
-double getMaxDouble(double c, double d);
+int getMax(int a, int b);
+double getMax(double c, double d);
   
 int main (){
     
@@ -18,19 +18,19 @@ int main (){
     cin >> c; 
 	cout <<"Escriba otro numero decimal" << endl;
     cin >> d;
-    cout <<"El numero entero mas grande es: " << getMaxInt(a,b) << endl;
-    cout << endl <<"El numero decimal mas grande es: " << getMaxDouble(c,d) << endl;
+    cout <<"El numero entero mas grande es: " << getMax(a,b) << endl;
+    cout << endl <<"El numero decimal mas grande es: " << getMax(c,d) << endl;
 	cout << endl;
     return 0; 
 } 
 
-int getMaxInt(int a, int b){
+int getMax(int a, int b){
 	if(a < b){
 		return b;
 	}
 }
 
-double getMaxDouble(double c, double d){
+double getMax(double c, double d){
 	if(c < d){
 		return d;
 	}
